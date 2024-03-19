602da35eb1bb0451563bcdbe8478613fef7e2d00
Rishabh Singh <RishabhSinghWeb@gmail.com>
2024-03-19 11:12:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from PIL import Image, ImageDraw, ImageFilter, ImageFont,ImageStat
+from PIL import Image, ImageDraw, ImageFilter, ImageFont
 import numpy as np
 from textwrap import wrap
-import getopt, sys, os, math
+import getopt, sys, os
 
 """
     This script uses a image to apply gradually blur on it
@@ -12,56 +12,6 @@ import getopt, sys, os, math
             OR
         python3 main.py -i input_image.png -o output_image.png -s bottom -t "text"
 """
-
-
-class Point(object):
-    def __init__(self, x, y):
-        self.x, self.y = x, y
-
-class Rect(object):
-    def __init__(self, x1, y1, x2, y2):
-        minx, maxx = (x1,x2) if x1 < x2 else (x2,x1)
-        miny, maxy = (y1,y2) if y1 < y2 else (y2,y1)
-        self.min = Point(minx, miny)
-        self.max = Point(maxx, maxy)
-
-    width  = property(lambda self: self.max.x - self.min.x)
-    height = property(lambda self: self.max.y - self.min.y)
-
-
-def gradient_color(minval, maxval, val, color_palette):
-    """ Computes intermediate RGB color of a value in the range of minval
-        to maxval (inclusive) based on a color_palette representing the range.
-    """
-    max_index = len(color_palette)-1
-    delta = maxval - minval
-    if delta == 0:
-        delta = 1
-    v = float(val-minval) / delta * max_index
-    i1, i2 = int(v), min(int(v)+1, max_index)
-    (r1, g1, b1, a1), (r2, g2, b2, a2) = color_palette[i1], color_palette[i2]
-    f = v - i1
-    return int(r1 + f*(r2-r1)), int(g1 + f*(g2-g1)), int(b1 + f*(b2-b1)), int(a1 + f*(a2-a1))
-
-def horz_gradient(draw, rect, color_func, color_palette):
-    minval, maxval = 1, len(color_palette)
-    delta = maxval - minval
-    width = float(rect.width)  # Cache.
-    for x in range(rect.min.x, rect.max.x+1):
-        f = (x - rect.min.x) / width
-        val = minval + f * delta
-        color = color_func(minval, maxval, val, color_palette)
-        draw.line([(x, rect.min.y), (x, rect.max.y)], fill=color)
-
-def vert_gradient(draw, rect, color_func, color_palette):
-    minval, maxval = 1, len(color_palette)
-    delta = maxval - minval
-    height = float(rect.height)  # Cache.
-    for y in range(rect.min.y, rect.max.y+1):
-        f = (y - rect.min.y) / height
-        val = minval + f * delta
-        color = color_func(minval, maxval, val, color_palette)
-        draw.line([(rect.min.x, y), (rect.max.x, y)], fill=color)
 
 def center_crop(img, w_ratio, h_ratio):        
     w, h = img.size
@@ -84,13 +34,13 @@ def center_crop(img, w_ratio, h_ratio):
 #     draw.rectangle([ (x0, y0 + rad), (x1, y1 - rad) ], fill=fill)
 #     draw.rectangle([ (x0 + rad, y0), (x1 - rad, y1) ], fill=fill)
 #     draw.pieslice([ (x0, y0), (x0 + rad * 2, y0 + rad * 2) ], 180, 270, fill=fill)
-#     draw.pieslice([ (x1 - rad * 2, y1 - rad * 2), (x1, y1) ], 0,    90, fill=fill)
-#     draw.pieslice([ (x0, y1 - rad * 2), (x0 + rad * 2, y1) ], 90,  180, fill=fill)
+#     draw.pieslice([ (x1 - rad * 2, y1 - rad * 2), (x1, y1) ], 0, 90, fill=fill)
+#     draw.pieslice([ (x0, y1 - rad * 2), (x0 + rad * 2, y1) ], 90, 180, fill=fill)
 #     draw.pieslice([ (x1 - rad * 2, y0), (x1, y0 + rad * 2) ], 270, 360, fill=fill)
 
-def display_text(display, image, text, h, w, font_path, font_size, margin, autofit_text = True, brightness=0):
+def display_text(display, image, text, h, w, font_path, font_size, margin, autofit_text = True):
     image_text_ratio = 0.3      # amount of screen covered with text 0.6 for 60%
-    text_shadow=50 # brightness//2
+    text_shadow=15
 
     if(type(font_size) == str):
         if font_size[-1] == '%':
@@ -125,7 +75,7 @@ def display_text(display, image, text, h, w, font_path, font_size, margin, autof
     for i in range(len(lines)):
         _,_, text_width, text_height = font.getbbox(lines[i])
         canvas = Image.new('RGBA', (text_width + font_margin*2, font_height),(0,0,0,text_shadow))
-        canvas2 = Image.new('RGBA', (int(text_width + font_margin*2)-int(font_size*0.7), font_height//2),(255,0,0,int(brightness//2)))
+        canvas2 = Image.new('RGBA', ((text_width + font_margin*2)-int(font_size*0.7), font_height//2),(255,0,0,80))
         draw = ImageDraw.Draw(image)
         height_offset = i*font_height
         width_offset = w-text_width
@@ -138,15 +88,13 @@ def display_text(display, image, text, h, w, font_path, font_size, margin, autof
         elif side == 'bottom':
             x, y = int(width_offset/2), h-(len(lines)-i)*font_height-margin
 
-        # if display == "shadow":
-        # blurred = canvas.filter(ImageFilter.GaussianBlur(20))
-        # blurred2 = canvas2.filter(ImageFilter.GaussianBlur(20))
-        # canvas2.paste(blurred2, mask=canvas2)
-        # canvas.paste(canvas2, (font_margin, font_size//2), canvas2)
-        # canvas.paste(blurred, mask=canvas)
-        # image.paste(canvas, (x, y), canvas)
-        # elif display == "text":
-        draw.text((x+font_margin,y+font_margin//2), lines[i], fill=font_color, font=font)
+        if display == "shadow":
+            blurred = canvas2.filter(ImageFilter.GaussianBlur(20))
+            canvas2.paste(blurred, mask=canvas2)
+            canvas.paste(canvas2, (font_size//2-2, font_size//2-2), canvas2)
+            image.paste(canvas, (x, y), canvas)
+        elif display == "text":
+            draw.text((x+font_margin,y+font_margin//2), lines[i], fill=font_color, font=font)
 
     return image
 
@@ -164,53 +112,20 @@ def gradularBlur(image, side, w, h, blur_accuracy, blur_scale):
             draw.rectangle([ (accuracy*i, 0), accuracy*(i+1)-1, h ], fill=fill)
         elif side == 'top' or side == 'bottom':
             draw.rectangle([ (0, accuracy*i), w, accuracy*(i+1)-1 ], fill=fill)
-        blurred = image.convert('RGB').filter(ImageFilter.GaussianBlur(blur_scale))
+        blurred = image.filter(ImageFilter.GaussianBlur(blur_scale))
         image.paste(blurred, mask=mask)
 
     return image
 
 def generate(image_path, side='bottom', text=None, font_size='7%', font_path='Fonts/OpenSans.ttf', blur_accuracy=20,
-             blur_scale=20, margin = "auto", autofit_text = True,light = 2):
+             blur_scale=20, margin = "auto", autofit_text = True):
     with Image.open(image_path) as image:
         image = center_crop(image, crop_ratio[0], crop_ratio[1])
-        # if(image.mode == "P"):
-        #     image=image.convert("L")
-        image=image.convert("RGBA")
+
         w,h = image.size
-        brightness = ImageStat.Stat(image).mean
-        brightness = sum(brightness)//len(brightness)
-
-        # #----- dark circular --------
-        # imgc = Image.new("RGBA", (200, 200), (0,0,0,0))
-        # ImageDraw.Draw(imgc).ellipse((20,20,180,180), fill=(0,0,0,150))
-        # imgc = imgc.filter(ImageFilter.GaussianBlur(10))
-        # imgc = imgc.resize((int(w*1.3),int(h*0.4)))
-        # wc,hc = imgc.size
-        # image.paste(imgc,(w//2 - wc//2,int(h - h*0.17 - hc/2)), mask=imgc)
-        # #---------------
-
-        # text shadow
-        # if text:
-        #     image = display_text(display="shadow",image=image, text=text, h=h, w=w, font_path=font_path, font_size=font_size, autofit_text=autofit_text, margin = margin, brightness=brightness)
-        
-        # # gradular blur
-        # image = gradularBlur(image=image, side=side, w=w, h=h, blur_accuracy=blur_accuracy, blur_scale=blur_scale)
-        
-        # #--- dark overlay  ----
-        # sides = ["top", "left", "bottom", "right"]
-        # dark_overlay = Image.open(["over.png", "over2.png", "over3.png"][light]).rotate(90*sides.index(side)).resize((w,h))
-        # image.paste(dark_overlay, (0,0), dark_overlay.convert("RGBA"))
-        # #---------------------
-
-        # #--- gradient ---------
-        # color_palette = [(200,0,0,255), (0,100,0,100)]
-        # region = Rect(0, 0, 730, 350)
-        # width, height = region.max.x+1, region.max.y+1
-        # draw = ImageDraw.Draw(image)
-        # vert_gradient(draw, region, gradient_color, color_palette)
-        # # ---------------------
-
-        # display text
+        if text:
+            image = display_text(display="shadow",image=image, text=text, h=h, w=w, font_path=font_path, font_size=font_size, autofit_text=autofit_text, margin = margin)
+        image = gradularBlur(image=image, side=side, w=w, h=h, blur_accuracy=blur_accuracy, blur_scale=blur_scale)
         if text:
             image = display_text(display="text",image=image, text=text, h=h, w=w, font_path=font_path, font_size=font_size, autofit_text=autofit_text, margin = margin)
 
@@ -220,7 +135,6 @@ def generate(image_path, side='bottom', text=None, font_size='7%', font_path='Fo
 if __name__ == '__main__':
     path = os.getcwd() + '/'
     # either use command line or these parameter
-    light = 0                                  # 0-black, 1, 2-light
     side = 'bottom'
     image_path = path + 'input_image.png'
     output_path = path + 'output_image.png'
@@ -260,7 +174,7 @@ if __name__ == '__main__':
             pass
 
     image = generate(image_path = image_path, side = side, text = text, font_size=font_size, font_path=font_path,
-                         margin = margin, autofit_text = autofit_text, light = light)
+                         margin = margin, autofit_text = autofit_text)
 
-    image.show()
-    # image.save(output_path)
+    # image.show()
+    image.save(output_path)
